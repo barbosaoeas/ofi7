9da08dea3bbf00a9e07c825a4d1586bcce48c6eb
Barbosaoeas <barbosa.eas@gmail.com>
2026-09-10 14:51:50 -0300

    wsgi.py: hardcode UAIZAPI_FINANCE_ALLOWED_CHAT_IDS/NAMES igual ao ZAP_WEBHOOK_SECRET (conta PA free nao tem env vars field no painel Web). Setup dos envs ANTES do get_wsgi_application para garantir disponibilidade no Django setup.

--- controle_oficina/wsgi.py
+++ controle_oficina/wsgi.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controle_oficina.settings')
 
-application = get_wsgi_application()
-
 os.environ["ZAP_WEBHOOK_SECRET"]="tk_18605d1680141f4301d8e16582441d28f0c9de8f"
+os.environ["UAIZAPI_FINANCE_ALLOWED_CHAT_IDS"]="120363040785758584@g.us"
+os.environ["UAIZAPI_FINANCE_ALLOWED_CHAT_NAMES"]="FINANCEIRO OFi7,financeiro"
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
